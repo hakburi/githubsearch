7488c4383bc0f2d1bd7d8e0ba73f11933fb4f4a4
SungHak <hakburi@naver.com>
2022-01-03 02:44:04 +0900

    Initialize project folder

---
+++ Project/GithubSearch/Sources/GithubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearchApp.swift
+//  GithubSearch
+//
+//  Created by SungHak Jung on 2022/01/03.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project/GithubSearch/Sources/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubSearch
+//
+//  Created by SungHak Jung on 2022/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
